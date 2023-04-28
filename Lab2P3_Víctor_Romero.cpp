b79e6584cbc7b28da6e47a3aa860151aa7fc891d
Víctor Romero <virn2005@gmail.com>
2023-04-28 15:31:20 -0600

    Ejercicio 1 Casi Listo

--- Lab2P3_Víctor_Romero.cpp
+++ Lab2P3_Víctor_Romero.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <locale>
+
 using namespace std;
+
+// Métodos 1er Ejercicio
+void Array() {
+	setlocale(LC_ALL, "spanish");
+	srand(time(NULL));
+
+	char arreglo[10];
+	char arreglo_Ordenado[10];
+
+	for (int i = 0; i < 10; i++) {
+		arreglo[i] = ((char)(97 + rand() % 25));
+	}
+
+	cout << "Arreglo Desordenado: ";
+	for (int i = 0; i < 10; i++) {
+		cout << arreglo[i] << "  ";
+	}
+	cout << endl << endl;
+
+	cout << "Arreglo Ordenado: ";
+	for (int i = 0; i < 10; i++) {
+		for (int j = i + 1; j < 10; j++) {
+			if (arreglo[i] > arreglo[j]) {
+				char temp = arreglo[i];
+				arreglo[i] = arreglo[j];
+				arreglo[j] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < 10; i++) {
+		cout << arreglo[i] << "  ";
+	}
+	cout << endl << endl;
+}
 
 // Métodos 2ndo Ejercicio
 int Factorial(int n) {
+	setlocale(LC_ALL, "spanish");
 	if (n == 0) { // Caso base
 		return 1;
 	}
@@ -12,6 +52,7 @@ int Factorial(int n) {
 }
 
 void Triangulo(int n) {
+	setlocale(LC_ALL, "spanish");
 	for (int row = 0; row < n; row++) {
 		for (int column = 0; column <= row; column++) {
 			cout << "*";
@@ -21,7 +62,9 @@ void Triangulo(int n) {
 	cout << endl;
 }
 
+// Menú en Method
 int menu() {
+	setlocale(LC_ALL, "spanish");
 	int opcion = 0;
 
 	do {
@@ -46,6 +89,7 @@ int menu() {
 		case 1: {
 			cout << "- 1) Ordenamiento y omisión en arreglos - \n";
 
+			Array();
 		}
 			  break;
 
@@ -53,8 +97,16 @@ int menu() {
 			cout << "- 2) Factoriales y figuras - \n";
 
 			cout << ">> Ingrese el Numero del Factorial: ";
-			int num;
+			int num = 0;
 			cin >> num;
+
+			do {
+				if (num < 0) {
+					cout << ">> Ingrese el Numero del Factorial VALIDO: ";
+					int num;
+					cin >> num;
+				}
+			} while (num < 0);
 
 			cout << "El factorial de " << num << " es " << num * Factorial(num - 1) << endl;
 			Triangulo(Factorial(num));
@@ -75,9 +127,12 @@ int menu() {
 }
 
 int main() {
+	setlocale(LC_ALL, "spanish");
+
 	cout << "Victor I. Romero N. - 12211079" << endl
 		<< "Lab 1 - Programacion 3" << endl << endl;
 
+	// Impresión de Menú
 	menu();
 
 }
